31a9b1b265d46468c7be64858a78c8693ca3f2bd
MuhammadAidil-dev <muhammad.aidil09767@gmail.com>
2025-06-20 10:42:32 +0700

    fix: show data stok

--- frontend/src/api/bloodStockApi.js
+++ frontend/src/api/bloodStockApi.js
@@ -4,8 +4,16 @@ import CONFIG from '../config/config';
 export const getAllData = async () => {
   try {
     const response = await fetchData(`${CONFIG.API_URL}/blood-stock`);
-    console.log(response);
+    if (response.status !== 'success') {
+      throw new Error(response.message || 'Gagal mengambil data stok');
+    }
+
+    return {
+      status: response.status,
+      message: response.message,
+      data: response.data,
+    };
   } catch (error) {
-    console.log(error);
+    return { status: 'error', message: error.message, data: null };
   }
 };
